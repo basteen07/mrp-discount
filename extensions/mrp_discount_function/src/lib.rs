44430ccfe6f53bbd0fa5681b5e8862bf2b6b6f69
basteen-Dev <basteen@digitfellas.com>
2026-01-14 23:40:04 +0530

    update

--- extensions/mrp_discount_function/src/lib.rs
+++ extensions/mrp_discount_function/src/lib.rs
@@ -3,10 +3,10 @@ use shopify_function::prelude::*;
 #[typegen("schema.graphql")]
 pub mod schema {
     #[query("src/cart_lines_discounts_generate_run.graphql")]
-    pub mod cart_lines_discounts_generate_run;
+    pub mod cart_lines_discounts_generate_run {}
 
     #[query("src/cart_delivery_options_discounts_generate_run.graphql")]
-    pub mod cart_delivery_options_discounts_generate_run;
+    pub mod cart_delivery_options_discounts_generate_run {}
 }
 
 mod cart_lines_discounts_generate_run;
